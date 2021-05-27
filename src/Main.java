b83454f4b7ca0b8d66334abf6b00d9292c7c50c9
SSinghNet <sumeet605@gmail.com>
2021-05-27 07:29:51 -0400

    initial people

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
-import static java.lang.System.out;
-
 import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        Contact person = new Contact("John", "Doe", "11232453", "123 Pine Street, Budd Lake, NJ, USA", "3 Pine Street, Budd Lake, NJ, USA");
-        Contact person2 = new Contact("John", "Doe", "11232453", "123 Pine Street, Budd Lake, NJ, USA",
+        Contact person = new Contact("John", "Doe", "11232453", "123 Main St, Word, NJ, USA", null);
+        Contact person2 = new Contact("Jane", "Doe", "2326123", "123 Pine Street, Towm, NJ, USA",
                 "3 Pine Street, Budd Lake, NJ, USA");
+        Contact person3 = new Contact("Person", "Name", "7321513", null, "78 Main Street, Town, Sate, USA");
         ArrayList<Contact> c = new ArrayList<Contact>();
         c.add(person);
         c.add(person2);
+        c.add(person3);
         AddressBook book = new AddressBook(c);
         
         book.run();
